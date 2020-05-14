72a6dd4523f6368691c3991005f570684930f4ca
dragazo <37459229+dragazo@users.noreply.github.com>
2020-05-14 00:32:02 -0500

    arbitrary tessellations
    added support for performing arbitrary tessellations.
    i'd considered doing this from the start but thought the automatic phase shifting would be tricky.
    but because some shapes have weird tessellation basis vectors, i've decided to not even try to calculate them.
    
    the input for the new 'geo' search option is a geometry file.
    first line is 4 integers representing two tessellation basis vectors.
    the rest of the file is a binary histogram where 1's denote in the geometry and 0 not in.
    
    the geometry can be literally anything, including both convex and concave shapes.
    it doesn't even have to be contiguous: you could put a hole in a diamond and as long as it's somehow filled in uniquely via tessellation you're good to go.
    that being said, there are some limitations with the current tessellation mapping algorithm (which would only be a problem for very weird shapes).
    for instance, it only attempts to perform a 5x5 tessellation.
    this might be too small or too much, so in the future this will be improved to be faster and work for every possible type of shape.
    
    from there it simply runs through every possible combination of old set contents and checks if they work.
    
    CURRENT_BEST is removed and replaced by a trailing (required) command line argument.
    
    added an Adjacent4 iterator for getting geometry boundaries, but i ended up not using it - will likely be removed in a future commit.
    
    added 2 example geometry files for reconstructing some known 1/4 diamond solutions.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, HashMap, HashSet};
 use std::fmt;
-
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+use std::path::Path;
 use rand::Rng;
-
-const CURRENT_BEST: f64 = 0.25;
 
 struct Adjacent {
     row: isize,
@@ -45,12 +45,39 @@ impl Iterator for Adjacent {
 }
 impl ExactSizeIterator for Adjacent {}
 
-struct KGrid {
-    rows: usize,
-    cols: usize,
-    old_set: Vec<bool>,
-    phase: (isize, isize),
+struct Adjacent4 {
+    row: isize,
+    col: isize,
+    state: usize,
 }
+impl Adjacent4 {
+    fn new(row: isize, col: isize) -> Self {
+        Self { row, col, state: 0 }
+    }
+}
+impl Iterator for Adjacent4 {
+    type Item = (isize, isize);
+    fn next(&mut self) -> Option<Self::Item> {
+        let r = self.row;
+        let c = self.col;
+
+        let v = match self.state {
+            0 => (r - 1, c),
+            1 => (r, c - 1),
+            2 => (r, c + 1),
+            3 => (r + 1, c),
+            _ => return None,
+        };
+        self.state += 1;
+        Some(v)
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let v = 4 - self.state;
+        (v, Some(v))
+    }
+}
+impl ExactSizeIterator for Adjacent4 {}
+
 struct SolverData {
     phases: Vec<(isize, isize)>,
     codes: BTreeSet<Vec<(isize, isize)>>,
@@ -58,7 +85,14 @@ struct SolverData {
     needed: usize,
     prevs: Vec<(isize, isize)>,
 }
-impl fmt::Display for KGrid {
+
+struct KGridRect {
+    rows: usize,
+    cols: usize,
+    old_set: Vec<bool>,
+    phase: (isize, isize),
+}
+impl fmt::Display for KGridRect {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for r in 0..self.rows {
             for c in 0..self.cols {
@@ -70,7 +104,7 @@ impl fmt::Display for KGrid {
         Ok(())
     }
 }
-impl fmt::Debug for KGrid {
+impl fmt::Debug for KGridRect {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let _r = self.rows as isize;
         let _c = self.cols as isize;
@@ -93,12 +127,12 @@ impl fmt::Debug for KGrid {
         Ok(())
     }
 }
-impl KGrid {
+impl KGridRect {
     fn new(rows: usize, cols: usize) -> Self {
-        assert_ne!(rows, 0);
-        assert_ne!(cols, 0);
+        assert!(rows >= 2);
+        assert!(cols >= 2);
 
-        KGrid {
+        KGridRect {
             rows, cols,
             old_set: vec![false; rows * cols],
             phase: (0, 0),
@@ -165,13 +199,14 @@ impl KGrid {
         }
         true
     }
-    fn calc_old_min(&mut self) -> Option<usize> {
+    fn calc_old_min(&mut self, thresh: f64) -> Option<usize> {
+        assert!(thresh > 0.0 && thresh <= 1.0);
         assert_eq!(self.old_set.len(), self.rows * self.cols);
         for x in &mut self.old_set {
             *x = false;
         }
 
-        let needed = (self.old_set.len() as f64 * CURRENT_BEST).ceil() as usize - 1;
+        let needed = (self.old_set.len() as f64 * thresh).ceil() as usize - 1;
         let mut data = SolverData {
             phases: {
                 let max_phase_x = (self.cols as isize + 1) / 2;
@@ -184,7 +219,7 @@ impl KGrid {
             prevs: Vec::with_capacity(needed),
         };
 
-        if self.calc_old_min_interior(&mut data, 0) { Some(data.needed) } else { None }
+        if self.calc_old_min_interior(&mut data, 0) { Some(needed) } else { None }
     }
     fn calc_old_min_interior(&mut self, data: &mut SolverData, pos: usize) -> bool {
         if data.needed == data.prevs.len() {
@@ -213,6 +248,196 @@ impl KGrid {
     }
 }
 
+#[derive(Debug)]
+enum GeometryLoadResult {
+    FileOpenFailure,
+    InvalidFormat,
+    TessellationFailure,
+}
+struct FancySolverData<'a> {
+    codes: BTreeSet<Vec<(isize, isize)>>,
+    needed: usize,
+    prevs: Vec<(isize, isize)>,
+
+    old_set: &'a mut BTreeSet<(isize, isize)>,
+    shape_with_padding: &'a BTreeSet<(isize, isize)>,
+    tessellation_map: &'a HashMap<(isize, isize), (isize, isize)>,
+}
+impl<'a> FancySolverData<'a> {
+    fn get_locating_code(&self, pos: (isize, isize)) -> Vec<(isize, isize)> {
+        let mut v = Vec::with_capacity(8);
+        for x in Adjacent::new(pos.0, pos.1) {
+            let mapped = self.tessellation_map.get(&x).unwrap();
+            if self.old_set.contains(mapped) {
+                v.push(x);
+            }
+        }
+        v
+    }
+    fn is_old(&mut self) -> bool {
+        self.codes.clear();
+        for pos in self.shape_with_padding {
+            let code = self.get_locating_code(*pos);
+            if code.is_empty() || !self.codes.insert(code) {
+                return false;
+            }
+        }
+        true
+    }
+    fn calc_old_min_interior<'b, P>(&mut self, mut pos: P) -> bool
+    where P: Iterator<Item = &'b (isize, isize)> + Clone
+    {
+        if self.needed == self.prevs.len() {
+            if self.is_old() {
+                return true;
+            }
+        } else if let Some(&p) = pos.next() {
+            self.old_set.insert(p);
+            self.prevs.push(p);
+            if self.calc_old_min_interior(pos.clone()) {
+                return true;
+            }
+            self.prevs.pop();
+            self.old_set.remove(&p);
+
+            return self.calc_old_min_interior(pos);
+        }
+
+        false
+    }
+}
+struct KGridFancyTessellation {
+    shape: BTreeSet<(isize, isize)>,
+    shape_with_padding: BTreeSet<(isize, isize)>,
+    tessellation_map: HashMap<(isize, isize), (isize, isize)>,
+    old_set: BTreeSet<(isize, isize)>,
+}
+impl fmt::Display for KGridFancyTessellation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut working_row = !0;
+        let mut working_col = 0;
+        for x in &self.shape {
+            if x.0 != working_row {
+                if working_row != !0 {
+                    for _ in 0..(x.0 - working_row) { writeln!(f)?; }
+                }
+                working_row = x.0;
+                working_col = 0;
+            }
+            for _ in 0..(x.1 - working_col) { write!(f, "  ")?; }
+            working_col = x.1 + 1;
+            write!(f, "{} ", if self.old_set.contains(x) { 1 } else { 0 })?;
+        }
+        writeln!(f)?;
+        Ok(())
+    }
+}
+impl KGridFancyTessellation {
+    fn with_shape<P: AsRef<Path>>(path: P) -> Result<Self, GeometryLoadResult> {
+        let mut f = BufReader::new(match File::open(path) {
+            Ok(x) => x,
+            Err(_) => return Err(GeometryLoadResult::FileOpenFailure),
+        });
+
+        let (tess_a, tess_b) = {
+            let mut line = String::new();
+            f.read_line(&mut line).unwrap();
+
+            let args: Vec<&str> = line.split_whitespace().collect();
+            if args.len() != 4 {
+                return Err(GeometryLoadResult::InvalidFormat);
+            }
+            let mut parsed: Vec<isize> = vec![];
+            for arg in args {
+                parsed.push(match arg.parse::<isize>() {
+                    Ok(x) => x,
+                    Err(_) => return Err(GeometryLoadResult::InvalidFormat),
+                });
+            }
+            ((parsed[0], parsed[1]), (parsed[2], parsed[3]))
+        };
+
+        let shape = {
+            let mut shape: BTreeSet<(isize, isize)> = Default::default();
+            for (row, line) in f.lines().map(|x| x.unwrap()).enumerate() {
+                for (col, item) in line.split_whitespace().enumerate() {
+                    match item.parse::<u8>() {
+                        Ok(x) if x <= 1 => {
+                            if x != 0 {
+                                shape.insert((row as isize, col as isize));
+                            }
+                        },
+                        _ => return Err(GeometryLoadResult::InvalidFormat),
+                    };
+                }
+            }
+            if shape.is_empty() {
+                return Err(GeometryLoadResult::InvalidFormat);
+            }
+            shape
+        };
+
+        let shape_with_padding: BTreeSet<_> = {
+            shape.clone().into_iter().flat_map(|x| Adjacent::new(x.0, x.1)).collect()
+        };
+        let shape_with_extra_padding: BTreeSet<_> = {
+            shape_with_padding.clone().into_iter().flat_map(|x| Adjacent::new(x.0, x.1)).collect()
+        };
+
+        let tessellation_map = {
+            let mut p: HashSet<(isize, isize)> = HashSet::with_capacity(shape.len() * 25);
+            let mut m: HashMap<(isize, isize), (isize, isize)> = HashMap::with_capacity(shape.len() * 9);
+
+            for &to in &shape {
+                for i in -2..=2 {
+                    for j in -2..=2 {
+                        let from = (to.0 + tess_a.0 * i + tess_b.0 * j, to.1 + tess_a.1 * i + tess_b.1 * j);
+                        if !p.insert(from) {
+                            return Err(GeometryLoadResult::TessellationFailure);
+                        }
+                        if shape_with_extra_padding.contains(&from) {
+                            m.insert(from, to);
+                        }
+                    }
+                }
+            }
+            for pos in &shape_with_extra_padding {
+                if !m.contains_key(pos) {
+                    return Err(GeometryLoadResult::TessellationFailure);
+                }
+            }
+
+            m
+        };
+
+        Ok(Self {
+            shape, shape_with_padding, tessellation_map,
+            old_set: Default::default(),
+        })
+    }
+    fn size(&self) -> usize {
+        self.shape.len()
+    }
+
+    fn calc_old_min(&mut self, thresh: f64) -> Option<usize> {
+        assert!(thresh > 0.0 && thresh <= 1.0);
+        self.old_set.clear();
+
+        let needed = (self.size() as f64 * thresh).ceil() as usize - 1;
+        let mut data = FancySolverData {
+            codes: Default::default(),
+            needed,
+            prevs: Vec::with_capacity(needed),
+
+            old_set: &mut self.old_set,
+            tessellation_map: &self.tessellation_map,
+            shape_with_padding: &self.shape_with_padding
+        };
+
+        if data.calc_old_min_interior(self.shape.iter()) { Some(needed) } else { None }
+    }
+}
+
 // source: https://doc.rust-lang.org/std/ops/trait.Div.html
 fn gcd(mut x: usize, mut y: usize) -> usize {
     while y != 0 {
@@ -231,7 +456,7 @@ fn main() {
     }
 
     {
-        let mut g = KGrid::new(4, 4);
+        let mut g = KGridRect::new(4, 4);
 
         assert_eq!(g.id_to_inside(0, 0), 0);
         assert_eq!(g.id_to_inside(1, 0), 4);
@@ -277,10 +502,24 @@ fn main() {
         assert_eq!(g.id_to_inside(-1, -1), 11);
     }
 
+    let parse_thresh = |a: &str| {
+        match a.parse::<f64>() {
+            Ok(v) if v > 0.0 && v <= 1.0 => v,
+            Ok(_) => {
+                eprintln!("thresh must be (0, 1]");
+                std::process::exit(6);
+            },
+            Err(_) => {
+                eprintln!("failed to parse thresh as a floating point value");
+                std::process::exit(7);
+            }
+        }
+    };
+
     match v[1].as_str() {
         "full" => {
-            if v.len() != 4 {
-                eprintln!("usage: {} full [rows] [cols]", v[0]);
+            if v.len() != 5 {
+                eprintln!("usage: {} full [rows] [cols] [thresh]", v[0]);
                 std::process::exit(2);
             }
 
@@ -293,22 +532,45 @@ fn main() {
                 eprintln!("1x1, 1xn, nx1 are not supported to avoid branch conditions\nthey also cannot result in lower than 2/3");
                 std::process::exit(3);
             }
+            let thresh = parse_thresh(v[4].as_str());
 
-            let mut grid = KGrid::new(rows, cols);
-            match grid.calc_old_min() {
+            let mut grid = KGridRect::new(rows, cols);
+            match grid.calc_old_min(thresh) {
                 Some(min) => {
                     let n = rows * cols;
                     let d = gcd(min, n);
                     println!("NEW BEST!! {}/{} ({}):\n{}", (min / d), (n / d), (min as f64 / n as f64), grid);
                 },
-                None => {
-                    println!("not better than {}", CURRENT_BEST);
-                }
+                None => println!("not better than {}", thresh),
+            };
+        },
+        "geo" => {
+            if v.len() != 4 {
+                eprintln!("usage: {} geo [geometry file] [thresh]", v[0]);
+                std::process::exit(2);
+            }
+
+            let mut geo = match KGridFancyTessellation::with_shape(&v[2]) {
+                Ok(x) => x,
+                Err(e) => {
+                    eprintln!("failed to load tessellation file {} ({:?})", v[2], e);
+                    std::process::exit(5);
+                },
+            };
+            let thresh = parse_thresh(v[3].as_str());
+
+            match geo.calc_old_min(thresh) {
+                Some(min) => {
+                    let n = geo.size();
+                    let d = gcd(min, n);
+                    println!("NEW BEST!! {}/{} ({}):\n{}", (min / d), (n / d), (min as f64 / n as f64), geo);
+                },
+                None => println!("not better than {}", thresh),
             };
         },
         "rand" => {
-            if v.len() != 4 {
-                eprintln!("usage: {} rand [rows] [cols]", v[0]);
+            if v.len() != 5 {
+                eprintln!("usage: {} rand [rows] [cols] [thesh]", v[0]);
                 std::process::exit(2);
             }
 
@@ -318,11 +580,12 @@ fn main() {
                 eprintln!("1x1, 1xn, nx1 are not supported to avoid branch conditions\nthey also cannot result in lower than 2/3");
                 std::process::exit(3);
             }
+            let thresh = parse_thresh(v[4].as_str());
 
-            let mut grid = KGrid::new(rows, cols);
+            let mut grid = KGridRect::new(rows, cols);
 
             let n: usize = rows * cols;
-            let k: usize = (n as f64 * CURRENT_BEST).ceil() as usize - 1;
+            let k: usize = (n as f64 * thresh).ceil() as usize - 1;
             assert!(n > k);
 
             let max_phase_x: isize = (cols as isize + 1) / 2;
